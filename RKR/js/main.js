ef4eb39e1328da12d554eece58278c5888b29e73
Ravi Raj Raut <46756880+ryavee@users.noreply.github.com>
2020-06-24 13:57:51 +0530

    first commit

---
+++ RKR/js/main.js
@@ -0,0 +1,120 @@
+(function($) {
+  
+  "use strict";
+
+
+
+  /* Books and Courses Carousel */
+    var owl = $("#testimonials");
+      owl.owlCarousel({
+        navigation: true,
+        pagination: true,
+        slideSpeed: 1000,
+        stopOnHover: true,
+        autoPlay: true,
+        items: 2,
+        itemsDesktop : [1199,2],
+        itemsDesktopSmall : [980,2],
+        itemsTablet: [768,1],
+        itemsTablet: [767,1],
+        itemsTabletSmall: [480,1],
+        itemsMobile : [479,1],
+      });   
+      var owl = $("#testimonials1");
+      owl.owlCarousel({
+        navigation: true,
+        pagination: true,
+        slideSpeed: 1000,
+        stopOnHover: true,
+        autoPlay: true,
+        items: 2,
+        itemsDesktop : [1199,2],
+        itemsDesktopSmall : [980,2],
+        itemsTablet: [768,1],
+        itemsTablet: [767,1],
+        itemsTabletSmall: [480,1],
+        itemsMobile : [479,1],
+      });
+
+
+/* 
+   Touch Owl Carousel */
+    var owl = $(".touch-slider");
+    owl.owlCarousel({
+      navigation: false,
+      pagination: true,
+      slideSpeed: 1000,
+      stopOnHover: true,
+      autoPlay: true,
+      items: 1,
+      itemsDesktopSmall: [1024, 1],
+      itemsTablet: [600, 1],
+      itemsMobile: [479, 1]
+    });
+
+    $('.touch-slider').find('.owl-prev').html('<i class="lni-chevron-left"></i>');
+    $('.touch-slider').find('.owl-next').html('<i class="lni-chevron-right"></i>');
+
+/* 
+   Sticky Nav*/
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.header-top-area').addClass('menu-bg');
+        } else {
+            $('.header-top-area').removeClass('menu-bg');
+        }
+    });
+
+
+/* 
+   Back Top Link*/
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    })
+
+    // one page navigation 
+    $('.main-navigation').onePageNav({
+            currentClass: 'active'
+    }); 
+
+    $(window).on('load', function() {
+       
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 200) {
+                $('.fixed-top').addClass('menu-bg');
+            } else {
+                $('.fixed-top').removeClass('menu-bg');
+            }
+        });
+
+    });
+
+/* 
+   Page Loader
+   ========================================================================== */
+   $(window).on('load',function() {
+      "use strict";
+      $('#loader').fadeOut();
+    });
+
+}(jQuery));
+
